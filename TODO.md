bd95346f9a693f973bcbcf707286036b385741f3
whiterqbbit <whiterqbbit@proton.me>
2023-11-28 13:55:19 +0100

    ✨ 404

--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
 # Design
 - [ ] Faire photo dans les couleurs
 - [ ] Trouver un pattern de fond
+
+# Pages
+- [ ] 404
---
+++ pages/[...slug].vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p class="text-mocha-white text-5xl font-black">404 frero</p>
+  </div>
+</template>
