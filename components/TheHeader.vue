df6f0bd98973e5f2be99387a614ccc5aaf5ce9b4
whiterqbbit <whiterqbbit@proton.me>
2023-11-29 02:04:27 +0100

    💅 add animations

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -14,14 +14,17 @@ watch(() => route.path, newPath => path.value = newPath)
     class="sticky top-0 z-50 transition-all ease-out duration-1000 rounded-b-3xl bg-ctp-mantle"
     :class="y > 0 ? 'bg-opacity-50 dark:bg-opacity-70 backdrop-blur-sm py-1' : 'bg-opacity-0 dark:bg-opacity-0 py-4'"
   >
-    <div name="container" class="flex max-w-5xl m-auto font-bold px-2 sm:px-4 md:px-6">
-      <NuxtLink to="/" class="flex gap-2 sm:gap-4">
-        <NuxtImg src="/catppuccin_logo.png" width="48" height="48" class="w-10 h-10 md:w-12 md:h-12 m-auto rounded-full hover:animate-bounce" />
-        <div class="m-auto sm:text-xl md:text-2xl text-sky emphasize">Guillaume Bonnefoy</div>
+    <div name="container" class="flex max-w-5xl m-auto px-2 sm:px-4 md:px-6">
+      <NuxtLink to="/" class="group flex gap-2 sm:gap-4">
+        <NuxtImg
+          src="/catppuccin_logo.png" width="48" height="48"
+          class="w-10 h-10 md:w-12 md:h-12 autoSlideIn m-auto rounded-full"
+        />
+        <div class="m-auto sm:text-xl md:text-2xl text-sky hover-target font-black">Guillaume Bonnefoy</div>
       </NuxtLink>
 
       <ul class="m-auto flex">
-        <li class="p-x2 sm:p-x3 flex gap-2 md:gap-6 text-sm sm:text-base md:text-lg">
+        <li class="flex gap-2 md:gap-6 font-bold text-sm sm:text-base md:text-lg">
           <NuxtLink to="/portfolio" exact-active-class="nav-link"> {{ $t('header.portfolio') }} </NuxtLink>
           <NuxtLink to="/blog" exact-active-class="nav-link"> {{ $t('header.blog') }} </NuxtLink>
           <NuxtLink to="/a-propos" exact-active-class="nav-link"> {{ $t('header.about') }} </NuxtLink>
@@ -37,19 +40,36 @@ watch(() => route.path, newPath => path.value = newPath)
 </template>
 
 <style scoped>
+.group:hover .hover-target {
+  @apply raise-color;
+}
+
 ul li * {
-  @apply hover-fast-transition;
+  @apply font-black;
 }
 
 ul li .nav-link {
   @apply text-anim-color font-black underline decoration-ctp-yellow/50 underline-offset-[6px] decoration-2;
 }
 
-.emphasize {
-  @apply hover:text-anim-color font-black hover:-translate-y-[3px];
+ul li *:hover {
+  @apply raise-color font-black;
 }
 
-ul li *:hover {
-  @apply emphasize;
+/* used by the logo */
+@keyframes slideInFromLeft {
+  0% {
+    opacity: 0;
+    transform: translate3d(-40px, 0, 0);
+  }
+  100% {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+}
+
+.autoSlideIn {
+  opacity: 0;
+  animation: slideInFromLeft 0.65s ease-out forwards;
 }
 </style>
