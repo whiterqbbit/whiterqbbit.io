ef9c6fe2622a56a081bd223462993afb00cd8375
whiterqbbit <whiterqbbit@proton.me>
2023-11-29 16:38:00 +0100

    ✨ add link to source

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,12 +1,18 @@
 <template>
-  <nav class="rounded-t-3xl">
-    <ul class="h-20 flex max-w-5xl m-auto">
+  <nav class="rounded-t-3xl h-20 flex flex-col gap-5 my-10">
+    <ul class="flex max-w-5xl mx-auto">
       <li class="m-auto flex gap-6 md:gap-8 text-3xl">
         <NuxtLink class="i-ci-mail" to="mailto:whiterqbbit@proton.me" target="_blank" />
         <NuxtLink class="i-ci-github" to="https://www.github.com/whiterqbbit" target="_blank" />
         <NuxtLink class="i-ci-linkedin" to="https://www.linkedin.com/in/white-rqbbit/" target="_blank" />
       </li>
     </ul>
+    <div class="text-center text-sm">
+      Fait par mes petites mains avec Nuxt.<br>
+      <NuxtLink class="hover:text-anim-color" to="https://www.github.com/whiterqbbit/whiterqbbit-website" target="_blank">
+        Code source sur Github
+      </NuxtLink>
+    </div>
   </nav>
 </template>
 
