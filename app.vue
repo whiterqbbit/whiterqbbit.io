eada52b6e7f5ccc04b6728f4b59f51e9fe097504
whiterqbbit <whiterqbbit@proton.me>
2023-11-29 13:29:59 +0100

    ✨ head metadata

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+useHead({
+  title: 'Guillaume Bonnefoy',
+  meta: [{ name: 'Guillaume Bonnefoy', content: 'Veni, Vidi, Codi' }],
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ctext y=\'.9em\' font-size=\'90\'%3E🧙‍♂️%3C/text%3E%3C/svg%3E' }],
+})
+
 const colorMode = useColorMode()
 const bg_color = computed(() => colorMode.preference === 'light' ? '#E6E9EF' : '#1E1E2E')
 </script>
