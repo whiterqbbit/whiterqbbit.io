baa4f3a893d3637e3f81fb110b3d002a6c8f60b8
whiterqbbit <whiterqbbit@proton.me>
2023-11-29 20:01:23 +0100

    ♻️ extract 404 into component

---
+++ components/NotFound.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+const { title, content } = defineProps({
+  title: String,
+  content: String,
+})
+</script>
+
+<template>
+  <div class="m-auto text-center flex place-content-center place-items-center u-spin-me-right-round">
+    <GlowCard class="u-spin-me-right-round">
+      <template #default>
+        <GlowCard class="u-spin-me-right-round">
+          <template #default>
+            <GlowCard class="u-spin-me-right-round">
+              <template #default>
+                <GlowCard class="u-spin-me-right-round">
+                  <template #default>
+                    <GlowCard class="u-spin-me-right-round">
+                      <template #default>
+                        <GlowCard class="u-spin-me-right-round">
+                          <template #default>
+                            <GlowCard class="u-spin-me-right-round">
+                              <template #default>
+                                <GlowCard :title="title" class="u-spin-me-right-round cursor-none">
+                                  <template #default>
+                                    {{ content }}
+                                  </template>
+                                </GlowCard>
+                              </template>
+                            </GlowCard>
+                          </template>
+                        </GlowCard>
+                      </template>
+                    </GlowCard>
+                  </template>
+                </GlowCard>
+              </template>
+            </GlowCard>
+          </template>
+        </GlowCard>
+      </template>
+    </GlowCard>
+  </div>
+</template>
+
+<style>
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+.u-spin-me-right-round:hover {
+  animation: spin 6s ease-in-out infinite;
+}
+</style>
---
+++ pages/blog/[...slug].vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <ContentDoc>
+      <template #not-found>
+        <NotFound title="404 frero" content="lol" />
+      </template>
+    </ContentDoc>
+  </main>
+</template>
