aa1f09714d6e16193009ce42e3762b9ec2788609
whiterqbbit <whiterqbbit@proton.me>
2023-11-29 18:01:54 +0100

    🩹 change NuxtImg folder

--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
+# Features
+- [ ] Umami / Matomo
+
 # Design
 - [ ] Faire photo dans les couleurs
 - [ ] Trouver un pattern de fond
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ export default defineNuxtConfig({
 
   image: {
     domains: ['whiterqbbit.io'],
-    dir: 'public',
+    dir: 'assets',
     format: ['webp'],
   },
 })
