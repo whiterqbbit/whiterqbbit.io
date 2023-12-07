12220764572f96aa20e4837cee6cb5867f72fc79
whiterqbbit <wedd13@gmail.com>
2023-12-07 01:01:20 +0100

    🎨 reorder projects and cosmetic adj

--- pages/portfolio.vue
+++ pages/portfolio.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { vue2, vue3, nuxt, tailwind, unocss, typescript, chrome, ethereum, node, nodets, prisma, react, sequelize, mongodb, elasticsearch, agile, aws_omniscient, aws_ttt, aws_calypso, terraform, docker, netlify, bootstrap, sass, html5, javascript, puppeteer, amplify, jest, vitest, cucumber, metabase } = icons
+const { vue2, vue3, nuxt, tailwind, unocss, typescript, chrome, ethereum, node, nodets, prisma, react, sequelize, mongodb, elasticsearch, aws_omniscient, aws_ttt, aws_calypso, terraform, docker, netlify, bootstrap, sass, html5, javascript, puppeteer, amplify, jest, vitest, cucumber } = icons
 
 interface IProject {
   name: string
@@ -13,54 +13,55 @@ interface IProject {
 
 const projects: IProject[] = [
   {
-    name: 'ChadGuard',
-    description: `<p>Extension Chrome utilisant IA pour bloquer le contenu adulte.</p>
-                  <p>~3000 utilisateurs actifs.</p>`,
-    tasks: 'Produit, développement fullstack, UI/UX',
-    stack: { front: [vue2, tailwind], back: [chrome] },
-    url: 'https://chromewebstore.google.com/detail/chadguard/oogpehhghgfaeojjbflgeemilhkhgbhe?hl=fr&pli=1',
-    source: 'https://github.com/LaGuerrePiece/ChadGuard',
+    name: 'Kafo',
+    description: 'Startup marketplace et rencontre coworking.',
+    tasks: 'Lead dev, produit, UI/UX, enquêtes utilisateurs, scrapping',
+    stack: { front: [vue3, unocss, typescript, vitest], back: [nodets, prisma, amplify] },
+    url: 'https://www.kafo.work/',
+    // source: 'https://github.com/whiterqbbit/kafo_front_vitesse'
   },
   {
     name: 'Moonolith',
-    description: `App web3 décentralisée axée sur la liberté d'expression.`,
-    tasks: 'Produit, développement fullstack, direction artistique, musique',
+    description: `<p>App web3 décentralisée.</p>
+                  <p>Moteur graphique maison, backend entièrement sur la blockchain.</p>`,
+    tasks: 'Dev, produit, direction artistique, musique',
     stack: { front: [html5, javascript], back: [ethereum] },
     url: 'https://www.moonolith.io/',
     source: 'https://github.com/LaGuerrePiece/moonolith',
   },
   {
-    name: 'Kafo',
-    description: 'Startup marketplace et rencontre coworking.',
-    tasks: 'Produit, développement fullstack, UI/UX, enquêtes utilisateurs',
-    stack: { front: [vue3, unocss, typescript, vitest], back: [nodets, prisma, amplify, agile] },
-    url: 'https://www.kafo.work/',
-    // source: 'https://github.com/whiterqbbit/kafo_front_vitesse'
-  },
-  {
-    name: 'Aestima-immo',
-    description: `Startup SaaS dans la gestion de patrimoine.`,
-    tasks: 'Développement fullstack',
-    stack: { front: [vue2, tailwind], back: [nodets, mongodb, docker, terraform, puppeteer] },
-    url: 'https://www.aestima-immo.com/',
+    name: 'ChadGuard',
+    description: `<p>Extension Chrome utilisant IA pour bloquer le contenu adulte.</p>
+                    <p>~3000 utilisateurs actifs.</p>`,
+    tasks: 'Dev, produit, UI/UX',
+    stack: { front: [vue2, tailwind], back: [chrome] },
+    url: 'https://chromewebstore.google.com/detail/chadguard/oogpehhghgfaeojjbflgeemilhkhgbhe?hl=fr&pli=1',
+    source: 'https://github.com/LaGuerrePiece/ChadGuard',
   },
   {
     name: 'My Omniscient',
     description: 'Startup IOT dans le secteur du BTP.',
-    tasks: 'Développement fullstack',
-    stack: { front: [vue2, bootstrap, sass], back: [node, aws_omniscient, elasticsearch, mongodb, cucumber, agile] },
+    tasks: `Dev fullstack: intégration de capteurs, développement d'outils backoffice`,
+    stack: { front: [vue2, bootstrap, sass], back: [node, aws_omniscient, elasticsearch, mongodb, cucumber] },
     url: 'https://myomniscient.com/solutions/application/',
   },
   {
+    name: 'Aestima-immo',
+    description: `Startup SaaS dans la gestion de patrimoine.`,
+    tasks: `Dev fullstack, refactor front d'une v1 et dev d'une v2`,
+    stack: { front: [vue2, tailwind], back: [nodets, mongodb, docker, terraform, puppeteer] },
+    url: 'https://www.aestima-immo.com/',
+  },
+  {
     name: 'Tictactrip',
-    description: 'Startup de moteur de recherche d\'itinéraire.',
-    tasks: 'Développement backend',
-    stack: { front: [react, tailwind, jest], back: [nodets, aws_ttt, terraform, sequelize, metabase, jest, agile] },
+    description: `Startup de moteur de recherche d'itinéraire.`,
+    tasks: `Dev backend, reverse-engineering d'apps`,
+    stack: { front: [react, tailwind, jest], back: [nodets, aws_ttt, terraform, sequelize, jest] },
     url: 'https://www.tictactrip.eu/',
   },
   {
     name: 'Calypso Bonnefoy',
-    description: 'Site vitrine d\'artiste style classique. Formulaire, mailing.',
+    description: `Site vitrine d'artiste de style classique. <br>Formulaire, mailing.`,
     stack: { front: [nuxt, tailwind], back: [aws_calypso, docker, terraform] },
     url: 'https://www.calypsobonnefoy.com/',
     source: 'https://github.com/whiterqbbit/calypso_bonnefoy/',
@@ -87,10 +88,11 @@ const projects: IProject[] = [
         class="max-w-xs overflow-visible"
       >
         <template #default>
-          <div class="cursor-pointer text-sm flex flex-col gap-2" @click="console.log('dab')" v-html="project.description" />
+          <div class="text-sm flex flex-col gap-1" v-html="project.description" />
           <div v-if="project.tasks" class="mt-2 text-sm italic text-ctp-overlay2"> {{ project.tasks }} </div>
-          <div class="flex flex-col mt-4 gap-2 text-2xl cursor-default">
+          <div class="flex flex-col mt-4 gap-2 text-lg cursor-default text-ctp-subtext2">
             <div class="flex flex-row gap-2">
+              <span class="text-sm ">Front :</span>
               <div v-for="tech in project.stack.front" :key="tech.name">
                 <UTooltip :text="tech.name">
                   <li :class="tech.icon" />
@@ -99,6 +101,7 @@ const projects: IProject[] = [
             </div>
 
             <div class="flex flex-row gap-2">
+              <span class="text-sm">Back :</span>
               <div v-for="tech in project.stack.back" :key="tech.name">
                 <UTooltip :text="tech.name">
                   <li :class="tech.icon" />
