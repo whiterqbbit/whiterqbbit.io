178893d1f7dd2d8a13fa711bfabd989f739edc6d
whiterqbbit <whiterqbbit@proton.me>
2023-12-09 11:58:27 +0100

    🎨 catppuccin tooltip colors

--- app.config.ts
+++ app.config.ts
@@ -12,7 +12,11 @@ export default defineAppConfig({
       'bg-ctp-mantle dark:bg-ctp-mantle text-ctp-text dark:text-ctp-text ring-ctp-surface0 dark:ring-ctp-surface0 focus:ring-ctp-lavender dark:focus:ring-ctp-lavender',
     } } },
     button: { variant: { solid:
-      'rounded-xl bg-anim-color-light dark:bg-anim-color-light hover:bg-anim-color hover:dark:bg-anim-color text-ctp-mantle dark:text-ctp-mantle font-bold transform transition duration-300 ease-out hover:scale-105 focus:ring-ctp-blue dark:focus:ring-ctp-blue',
+      'rounded-xl bg-anim-color-light dark:bg-anim-color-light hover:bg-anim-color hover:dark:bg-anim-color text-ctp-mantle dark:text-ctp-mantle font-bold transform transition-fast hover:scale-105 focus:ring-ctp-blue dark:focus:ring-ctp-blue',
     } },
+    tooltip: {
+      background: 'bg-ctp-mantle dark:bg-ctp-mantle',
+      color: 'text-ctp-text dark:text-ctp-text',
+    },
   },
 })
