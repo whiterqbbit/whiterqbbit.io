33375f34f42f9af48da35b71e849dd453a3842a0
whiterqbbit <whiterqbbit@proton.me>
2023-12-10 01:19:56 +0100

    🎨 change icon to🐇

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@ import { topography } from 'hero-patterns'
 useHead({
   title: 'Guillaume Bonnefoy',
   meta: [{ name: 'Guillaume Bonnefoy', content: 'Veni, Vidi, Codi' }],
-  link: [{ rel: 'icon', type: 'image/svg+xml', href: 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ctext y=\'.9em\' font-size=\'90\'%3E🧙‍♂️%3C/text%3E%3C/svg%3E' }],
+  link: [{ rel: 'icon', type: 'image/svg+xml', href: 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ctext y=\'.9em\' font-size=\'90\'%3E🐇%3C/text%3E%3C/svg%3E' }],
 })
 
 const colorMode = useColorMode()
