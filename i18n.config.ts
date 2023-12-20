06edcf13f8d4fc216afbe9091116f473bae72123
whiterqbbit <whiterqbbit@proton.me>
2023-12-20 21:43:44 +0100

    ✨ add i18n for landing page

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,4 +1,5 @@
 import { TheHeader } from '~/locales/components/TheHeader'
+import { landing } from '~/locales/pages'
 
 export default defineI18nConfig(() => ({
   legacy: false,
@@ -6,10 +7,12 @@ export default defineI18nConfig(() => ({
     en: {
       flag: '🇺🇸',
       header: TheHeader.en,
+      landing: landing.en,
     },
     fr: {
       flag: '🇫🇷',
       header: TheHeader.fr,
+      landing: landing.fr,
     },
   },
 }))
--- pages/index.vue
+++ pages/index.vue
@@ -36,48 +36,48 @@ function useEaster() {
         class="flex flex-col text-center w-1/2 gap-4 justify-round place-content-center slide-enter"
       >
         <div class="text-xl sm:text-3xl">
-          Hello ! Je suis <span class="text-anim-color font-bold text-xl sm:text-4xl">Guillaume Bonnefoy</span>
+          {{ $t('landing.intro.hello') }}
+          <span class="text-anim-color font-bold text-xl sm:text-4xl">Guillaume Bonnefoy</span>
         </div>
         <p class="text-ctp-subtext1">
-          <strong>Développeur fullstack</strong> et designer graphique,
-          spécialisé dans la création d'app early-stage
+          <strong>{{ $t('landing.intro.fullstack') }}</strong>
+          {{ $t('landing.intro.designer') }}
         </p>
       </div>
     </div>
 
     <div class="card-duo" :style="{ '--enter-stage': 2 }">
       <div class="sm:w-1/2 place-self-center">
-        Je suis actuellement : <span class="text-anim-color text-xl sm:text-2xl italic font-bold sm:pr-1">disponible !</span><br><br>
-        J'aime travailler sur des projets <strong>ambitieux</strong> et <strong>challengeants</strong>!<br>
-        Si votre vision est claire, je peux vous livrer le design et le développement de votre app en un
-        <strong>temps record</strong> !
+        {{ $t('landing.availability.main') }} <span class="text-anim-color text-xl sm:text-2xl italic font-bold sm:pr-1">{{ $t('landing.availability.yes') }}</span><br><br>
+        {{ $t('landing.work_preference.main') }} <strong>{{ $t('landing.work_preference.ambitious') }}</strong> {{ $t('landing.work_preference.and') }} <strong> {{ $t('landing.work_preference.challenging') }}</strong> !<br>
+        {{ $t('landing.vision') }} <strong>{{ $t('landing.record_time') }}</strong> !
         <!-- Mon TJM est de <span class="text-anim-color font-bold pr-1">400€</span><br> -->
       </div>
-      <GlowCard title="Contactez-moi !" class="m-auto sm:w-1/2">
+      <GlowCard :title="$t('landing.contact_me')" class="m-auto sm:w-1/2">
         <ContactForm />
       </GlowCard>
     </div>
 
     <div class="card-duo" :style="{ '--enter-stage': 3 }">
-      <GlowCard title="Ma stack" class="place-self-center max-w-sm sm:max-w-none sm:w-1/2">
+      <GlowCard :title="$t('landing.my_stack')" class="place-self-center max-w-sm sm:max-w-none sm:w-1/2">
         <TechStack />
       </GlowCard>
       <div class="sm:w-1/2 place-self-center">
-        Techniquement, j'ai un faible pour l'écosystème <strong>Vue/Nuxt</strong>, qui allie rapidité et scalabilité, ainsi que <strong>Typescript</strong>.<br>
-        Je privilégie les techniques <strong>modernes</strong>, mais <strong>matures</strong>. Évidemment, tout est fonction du projet !
+        {{ $t('landing.technical.main') }} <strong>Vue/Nuxt</strong>{{ $t('landing.technical.speed') }} <strong>Typescript</strong>.<br>
+        {{ $t('landing.technical.approach') }} <strong>{{ $t('landing.technical.modern') }}</strong>, {{ $t('landing.technical.yet') }} <strong>{{ $t('landing.technical.mature') }}</strong>{{ $t('landing.technical.everything') }}
       </div>
     </div>
 
     <div class="card-duo" :style="{ '--enter-stage': 4 }">
       <div class="sm:w-1/2 place-self-center">
-        Si votre projet le nécessite, je peux également solliciter mes amis au travers de notre collectif de freelance
+        {{ $t('landing.collective.main') }}
         <span class="text-anim-color text-lg sm:text-2xl italic font-bold sm:pr-1">Vite!</span>.<br>
-        C'est particulièrement pertinent pour les projets crypto/web3/IA, ou simplement si vous voulez avancer encore plus vite.
+        {{ $t('landing.collective.relevance') }}
       </div>
       <img src="https://media1.tenor.com/m/w61O0eYj-M4AAAAC/frens.gif" class="h-52 hidden sm:block object-cover rounded-3xl">
     </div>
     <div class="flex slide-enter" :style="{ '--enter-stage': 5 }">
-      <GlowCard title="Contactez-moi !" class="w-full max-w-sm sm:max-w-md sm:my-16 m-auto" emphasize>
+      <GlowCard :title="$t('landing.contact_me')" class="w-full max-w-sm sm:max-w-md sm:my-16 m-auto" emphasize>
         <ContactForm />
       </GlowCard>
     </div>
