d48a618cb6e2e53309e95f5e7167e199cf9b21e9
whiterqbbit <whiterqbbit@proton.me>
2023-12-21 14:26:10 +0100

    🔧 fix whole GlowCard clickable

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,15 +4,17 @@
 <template>
   <div class="flex flex-col gap-2 m-auto place-content-center">
     <ContentList v-slot="{ list }" path="/blog" class="">
-      <GlowCard
-        v-for="post, idx in list" :key="post._path" :title="post.title" :style="{ '--enter-stage': idx, '--enter-step': '130ms' }"
-        class="bg-ctp-surface0 rounded-2xl flex max-w-md place-self-center flex-col p-10 m-12 cursor-pointer slide-enter"
-      >
+      <div v-for="post, idx in list" :key="post._path">
         <NuxtLink :to="post._path">
-          <NuxtImg v-if="post.cover" :src="post.cover" class="rounded-2xl" />
-          <div class="text-lg text-ctp-flamingo"> {{ post.description }}</div>
+          <GlowCard
+            :title="post.title" :style="{ '--enter-stage': idx, '--enter-step': '130ms' }"
+            class="bg-ctp-surface0 rounded-2xl flex max-w-md place-self-center flex-col p-8 m-4 cursor-pointer slide-enter"
+          >
+            <NuxtImg v-if="post.cover" :src="post.cover" class="rounded-2xl" />
+            <div class="text-lg text-ctp-flamingo"> {{ post.description }}</div>
+          </GlowCard>
         </NuxtLink>
-      </GlowCard>
+      </div>
     </ContentList>
   </div>
 </template>
